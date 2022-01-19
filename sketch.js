1cfeaf10b5b51fdc5154b16c605be9a71b294670
ApnB21 <93769368+ApnB21@users.noreply.github.com>
2022-01-19 17:18:11 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -19,22 +19,34 @@ function setup() {
 
   playerBase = new PlayerBase(300, random(450, height - 300), 180, 150);
   player = new Player(285, playerBase.body.position.y - 153, 50, 180);
+  playerArcher = new PlayerArcher(
+    340,
+    playerBase.body.position.y - 180,
+    120,
+    120
+  );
 
-  playerArcher = new PlayerArcher(width - 600, playerBase.body.position.y - 180, 120, 120);
-
-  computerBase = new ComputerBase(width - 300,random(450, height - 300),180,150);
-  
+  computerBase = new ComputerBase(
+    width - 300,
+    random(450, height - 300),
+    180,
+    150
+  );
   computer = new Computer(
     width - 280,
     computerBase.body.position.y - 153,
     50,
     180
   );
-  computerArcher = new ComputerArcher(width - 340,computerBase.body.position.y - 180,120,120);
+  computerArcher = new ComputerArcher(
+    width - 340,
+    computerBase.body.position.y - 180,
+    120,
+    120
+  );
   
-  playerArrow = new PlayerArrow()
+  arrow = new PlayerArrow(playerArcher.body.position.x, playerArcher.body.position.y, 100, 10);
   
-  computerArrow = new ComputerArrow()
 }
 
 function draw() {
@@ -58,27 +70,8 @@ function draw() {
   
   playerArcher.display();
   computerArcher.display()
-
-
-  //Display arrow();
-  
-  
-
+  arrow.display()
   if(keyCode === 32){
-    arrow.shooot(playerArcher.body,shoot);
-
-    shoot(archerAngle){
-      var velocity = p5.vector.fromAngle(archerAngle);
-      velocity.mult(20);
-      Matter.Body.setStatic(this.body, false);
-      Matter.Body.setVelocity(this.body,{ x:velocity.x, y:velocity.y})
-    }
-
+   arrow.shoot(playerArcher.body.angle);
   }
-  
-
-  }
-
-
-
-
+}
